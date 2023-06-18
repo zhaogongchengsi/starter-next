6ba69d7b3539465a6e65e1470ffcca3243d3a592
zhaogongchengsi <zzh1586169624@163.com>
2023-06-18 23:01:27 +0800

    feat: 自定义侧边栏

--- layout/admin/Aside.tsx
+++ layout/admin/Aside.tsx
@@ -1,51 +1,48 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/siderbar";
-import data, { NavBarOptions } from "./data";
 import { cn } from "@/lib/utils";
-import Link from "next/link";
+import data from "./data";
 import React from "react";
-
-const AsideItem: React.FC<NavBarOptions & { indent?: number }> = (props) => {
-  const { link, icon, title, children, indent } = props;
-  const headerClassName = "w-full flex items-center py-4";
-  const titleClassName = "w-25 text-left ml-2 text-sm";
-
-  if (!children) {
-    return (
-      <Link
-        href={link || "/"}
-        className={cn("app-sider-border-t", headerClassName)}
-        style={{ paddingLeft: `${indent}px` }}
-      >
-        {icon && <div className={cn("w-5 h-5", icon)} />}
-        <span className={titleClassName}>{title}</span>
-      </Link>
-    );
-  }
-
-  return (
-    <AccordionItem value={title}>
-      <AccordionTrigger className="pr-3">
-        <div className={cn(headerClassName)} style={{ paddingLeft: `${indent}px` }}>
-          {icon && <div className={cn("w-5 h-5", icon)} />}
-          <span className={titleClassName}>{title}</span>
-        </div>
-      </AccordionTrigger>
-      <AccordionContent>
-        {children.map((child) => {
-          return <AsideItem {...child} indent={indent + 20} />;
-        })}
-      </AccordionContent>
-    </AccordionItem>
-  );
-};
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 const AdminAside: React.FC = () => {
   return (
-    <Accordion type="single" collapsible className="w-50">
-      {data.map((item) => {
-        return <AsideItem {...item} indent={10} />;
-      })}
-    </Accordion>
+    <Sidebar
+      rootStyles={{
+        color: "var(--app-foreground)",
+      }}
+      backgroundColor="var(--app-background)"
+    >
+      <Menu
+        menuItemStyles={{
+          button: {
+            background: "var(--app-background)",
+            "&:hover": {
+              backgroundColor: "var(--app-background-hover)",
+              color: "var(--app-foreground-hover)",
+            },
+          },
+        }}
+		renderExpandIcon={({ open }) => {
+			return <div className={cn('w-5 h-5 i-tabler-arrow-badge-right transition-all', open ? 'rotate-90' : 'rotate-0')} />
+		}}
+      >
+        {data.map(({ children, title, link, icon }) => {
+          if (children) {
+            return (
+              <SubMenu label={title} key={title} icon={<div className={cn("w-5 h-h", icon)} />}>
+                {children.map((child) => {
+                  return <MenuItem key={child.link}> {child.title} </MenuItem>;
+                })}
+              </SubMenu>
+            );
+          }
+          return (
+            <>
+              <MenuItem key={link}> {title} </MenuItem>
+            </>
+          );
+        })}
+      </Menu>
+    </Sidebar>
   );
 };
 
--- layout/admin/Header.tsx
+++ layout/admin/Header.tsx
@@ -1,11 +1,16 @@
-import useMode from "@/hooks/use-mode"
-
+import useMode from "@/hooks/use-mode";
 
 const AdminHeader = () => {
+  const [mode, setMode] = useMode();
 
-	const [ mode, setMode ] = useMode()
+  return (
+    <header className="h-[--admin-header-height] flex justify-between px-3 items-center">
+      <div className="w-5 h-5 i-tabler-arrows-left" />
+      <button onClick={() => setMode(!mode)}>
+        <div className="w-5 h-5 i-tabler-sun dark:i-tabler-moon" />
+      </button>
+    </header>
+  );
+};
 
-	return <header className="h-[--admin-header-height] flex justify-between">  <button onClick={() => setMode(!mode)} >mode</button> </header>
-}
-
-export default AdminHeader
+export default AdminHeader;
