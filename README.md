7622cdb3ae9aa8e0fb42492e05d2bbbdc21abf39
zhaogongchengsi <zzh1586169624@163.com>
2023-06-26 17:58:07 +0800

    登录页面初步完成

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - [ui shadcn](https://ui.shadcn.com/docs/installation)
 - [prisma database](https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/relational-databases/using-prisma-migrate-typescript-mysql)
 - [自定义侧边栏](https://github.com/azouaoui-med/react-pro-sidebar)
+- [react-hook-form](https://react-hook-form.com/docs/useform/resetfield)
--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -2,14 +2,17 @@
 import { Form, FormField, FormItem, FormLabel, FormControl, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import useMode from "@/hooks/use-mode";
+
+// @ts-ignore
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
-  account: z.string().min(2).max(50),
-  password: z.string().min(6).max(16),
+  account: z.string().min(5, { message: "账号长度不小于5位" }).max(15, { message: "账号长度不大于15位" }).trim(),
+  password: z.string().min(6, { message: "密码不小于6位" }).max(16, { message: "密码不大于16位" }).trim(),
+  captcha: z.string().length(4, { message: "验证码必填" }).trim(),
 });
 
 const Login = () => {
@@ -21,11 +24,10 @@ const Login = () => {
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      account: "",
-      password: "",
-    },
   });
+
+  // const { register } = form;
+  // const inputField = register("inputField", { required: "请输入账号" });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
@@ -47,32 +49,46 @@ const Login = () => {
                 control={form.control}
                 name="account"
                 render={({ field }) => (
-                  <>
-                    <FormItem>
-                      <FormLabel>账号</FormLabel>
-                      <FormControl>
-                        <Input placeholder="请输入账号" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  </>
+                  <FormItem>
+                    <FormLabel>账号</FormLabel>
+                    <FormControl>
+                      <Input type="text" placeholder="请输入账号" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
                 )}
               />
               <FormField
                 control={form.control}
                 name="password"
                 render={({ field }) => (
-                  <>
-                    <FormItem>
-                      <FormLabel>密码</FormLabel>
-                      <FormControl>
-                        <Input type="password" placeholder="请输入账号" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  </>
+                  <FormItem>
+                    <FormLabel>密码</FormLabel>
+                    <FormControl>
+                      <Input type="password" autoComplete="password" placeholder="请输入账号" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
                 )}
               />
+
+              <FormField
+                control={form.control}
+                name="captcha"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>验证码</FormLabel>
+                    <FormControl>
+                      <div className="w-full space-x-2 flex">
+                        <Input type="captcha" placeholder="请输入验证码" {...field} />
+                        <div className="w-40 rounded-md bg-background border border-input cursor-pointer">asd</div>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
               <Button className="w-full" type="submit">
                 登录
               </Button>
