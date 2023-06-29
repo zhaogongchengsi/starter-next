caa9b5b3d5d9ac9d22711b5594826b668c493538
zhaogongchengsi <zzh1586169624@163.com>
2023-06-29 17:27:23 +0800

    feat: 优化表单错误提示

--- schemas/login.ts
+++ schemas/login.ts
@@ -1,5 +1,5 @@
 import * as z from "zod";
 
-export const account = z.string().min(5, { message: "账号长度不小于5位" }).max(15, { message: "账号长度不大于15位" }).trim()
-export const password = z.string().min(6, { message: "密码不小于6位" }).max(16, { message: "密码不大于16位" }).trim()
-export const captcha = z.string().length(4, { message: "验证码必填且长度为4位" }).trim()
+export const account = z.string({ required_error: "请填写账号" }).min(5, { message: "账号长度不小于5位" }).max(15, { message: "账号长度不大于15位" }).trim()
+export const password = z.string({ required_error: "请填写密码" }).min(6, { message: "密码不小于6位" }).max(16, { message: "密码不大于16位" }).trim()
+export const captcha = z.string({ required_error: "请填写验证码" }).length(4, { message: "验证码必填且长度为4位" }).trim()
